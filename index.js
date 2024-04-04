2cc6f9c77892ceb678f255bba046923a29dfee29
Buddha Lo <lo_977@yahoo.com>
2024-04-04 13:42:00 -0700

    complete

---
+++ index.js
@@ -0,0 +1,19 @@
+function Scooter(year, color, model) {
+  this.year = year;
+  this.color = color;
+  this.model = model;
+}
+const newScooter = new Scooter();
+
+function Driver(name, age, experience) {
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
+const newDriver = new Driver();
+
+function PickupLocation(address, city) {
+  this.address = address;
+  this.city = city;
+}
+const newPickupLocation = new PickupLocation();
